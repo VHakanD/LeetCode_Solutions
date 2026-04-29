8cf605b3af29e908665bd6d2bf5b38d4b3e03404
Hakan Demir <hvehbi78@gmail.com>
2026-04-29 21:56:10 +0300

    0 ms (100%) 10.5 MB (44.22%) 04-29-2026 0003-longest-substring-without-repeating-characters Medium .c Hash Table

--- LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
+++ LeetCode/Medium/0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -1,16 +1,19 @@
 int lengthOfLongestSubstring(char* s) {
-    int text[128] = {0};
+    int text[128];
+    for (int i = 0; i < 128; i++) {
+        text[i] = -1;
+    }
     int n=strlen(s);
     int maxLen=0, currLen=0, right=0, left=0;
-    
+
+    if (n == 0) return 0;
     while(right < n) {
         char str = *(s+right);
-        while(text[(int)str] == 1) {
-            text[(int)s[left]] = 0;
-            left++;
+        if(text[str] >= left ) {
+            left = text[str]+1;
         }
-        text[(int)str] = 1;
-        currLen = right-left+1;
+        text[str] = right;
+        currLen = right - left + 1;
         if(currLen > maxLen) {
             maxLen = currLen;
         }
