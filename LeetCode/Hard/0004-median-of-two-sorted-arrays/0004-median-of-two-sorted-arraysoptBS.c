022282b6c614a3426228e6bca68bcc1b339523a6
Hakan Demir <hvehbi78@gmail.com>
2026-04-30 01:01:15 +0300

    0 ms (100%) 11.9 MB (85.06%) 04-30-2026 0004-median-of-two-sorted-arrays Hard .c Array

---
+++ LeetCode/Hard/0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arraysoptBS.c
@@ -0,0 +1,36 @@
+int maxx(int a, int b) { return (a > b) ? a : b; }
+
+int min(int a, int b) { return (a < b) ? a : b; }
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    if (nums1Size > nums2Size) {
+        return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
+    }
+
+    int m = nums1Size, n = nums2Size;
+    int left = 0, right = m;
+
+    while (left <= right) {
+        int partitionA = (left + right) / 2;
+        int partitionB = (m + n + 1) / 2 - partitionA;
+
+        int maxLeftA = (partitionA == 0) ? INT_MIN : nums1[partitionA - 1];
+        int minRightA = (partitionA == m) ? INT_MAX : nums1[partitionA];
+        int maxLeftB = (partitionB == 0) ? INT_MIN : nums2[partitionB - 1];
+        int minRightB = (partitionB == n) ? INT_MAX : nums2[partitionB];
+
+        if (maxLeftA <= minRightB && maxLeftB <= minRightA) {
+            if ((m + n) % 2 == 0) {
+                return (maxx(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
+            } else {
+                return maxx(maxLeftA, maxLeftB);
+            }
+        } else if (maxLeftA > minRightB) {
+            right = partitionA - 1;
+        } else {
+            left = partitionA + 1;
+        }
+    }
+
+    return 0.0;
+}
