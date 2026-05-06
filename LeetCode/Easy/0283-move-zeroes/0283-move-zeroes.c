dfd94153154143daeb1567e0398da9aed3ca8f39
Hakan Demir <hvehbi78@gmail.com>
2026-05-07 00:56:00 +0300

    0 ms (100%) 20.4 MB (8.72%) 05-07-2026 0283-move-zeroes Easy .c Array

--- LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.c
+++ LeetCode/Easy/0283-move-zeroes/0283-move-zeroes.c
@@ -1,27 +1,17 @@
 void moveZeroes(int* nums, int numsSize) {
-    int i, j=0, numZero=0;
-    int *temp = nums;
-    if(numsSize == 1) return ;
+    int i, j = 0;
 
-    for(i=0;i<numsSize;i++) {
-        if(*(nums+i) == 0) {
-            numZero += 1;
-        }
-    }
+    if(numsSize <= 1) return;
 
-    for(i=0;i<numsSize;i++) {
-        if(*(nums+i) != 0) {
-            *(temp+j) = *(nums+i);
+    for(i = 0; i < numsSize; i++) {
+        if(nums[i] != 0) {
+            nums[j] = nums[i];
             j++;
         }
     }
-    
-    while(j < numsSize) {
-        *(temp+j) = 0;
-        j++;
-    }
 
-    for(i = 0; i < numsSize; i++) {
-        *(nums+i) = *(temp+i);
+    while(j < numsSize) {
+        nums[j] = 0;
+        j++;
     }
 }
